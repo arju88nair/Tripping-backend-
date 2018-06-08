d09d6288ee4250adc1b1febc07b9f41a92b3da7b
arju88nair <arju88nair@gmail.com>
2018-06-08 11:21:59 +0530

    proper chat test 1 single chat room with leaving session

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-const express = require('express')
-const app = express()
+/////////////////////////////////
+//  Import everything necessary
+
+var express = require('express')
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+// List of all connected users
+connectedUsers = [];
+
+// Express is using the 'public' folder (for accessing style.css)
+app.use(express.static('public'));
+
+// Declare which port server is listening too
+server.listen(3000);
+console.log('Server started.');
 
 
-//set the template engine ejs
-app.set('view engine', 'ejs')
+////////////////////////
+//  Declare all events
 
-//middlewares
-app.use(express.static('public'))
-
-
-//routes
-app.get('/', (req, res) => {
-	res.render('index')
-})
-
-//Listen on port 3000
-server = app.listen(3000)
+io.sockets.on('connection' , clientConnected);
 
 
+//////////////////
+// Event Handlers
 
-//socket.io instantiation
-const io = require("socket.io")(server)
+function clientConnected(socket){
+    // New connected user
+    socket.nickname = '';   // Not 'logged in' yet , not nickname associated
+    console.log('Client  ' + socket.handshake.address + '  connected.');
 
+    // Client disconnected
+    socket.on('disconnect' , function (){
+        console.log('Client  ' + socket.handshake.address + '  disconnected.');
 
-//listen on every connection
-io.on('connection', (socket) => {
-	console.log('New user connected')
+        // Remove the user that disconnected, if it was 'logged in'
+        if (socket.nickname != ''){
+            // Notify the other Users
+            socket.broadcast.emit('system message' , 'User <b>' + socket.nickname + '</b> has left the chat-room.');
 
-	//default username
-	socket.username = "Anonymous"
+            connectedUsers.splice(connectedUsers.indexOf(socket),1);
+            updateConnectedUsers();
+        }
+    });
 
-    //listen on change_username
-    socket.on('change_username', (data) => {
-        socket.username = data.username
-    })
+    // Broadcast the message to all clients
+    socket.on('send message' , sendMessage);
 
-    //listen on new_message
-    socket.on('new_message', (data) => {
-        //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-    })
+    // Client chose nickname
+    socket.on('new client nickname', function(nickname){
+        // Add the new client to the sockets list
+        socket.nickname = nickname;
+        connectedUsers.push(socket);
 
-    //listen on typing
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
-})
+        // Notify other clients only that this new clients has joined the chat-room
+        socket.broadcast.emit('system message' , 'User <b>' + socket.nickname + '</b> has joined the chat-room.');
+
+        updateConnectedUsers();
+    });
+}
+
+function sendMessage(message){
+    // Broadcast the message to all clients
+    io.sockets.emit('new message' , message);
+}
+
+function updateConnectedUsers(){
+    // Make a list of all the clients nicknames
+    nicknames = [];
+    for (i=0 ; i<connectedUsers.length ; i++){
+        nicknames.push(connectedUsers[i].nickname);
+    }
+
+    // Tell all the clients which clients are currently in the chat-room
+    io.sockets.emit('update current users' , nicknames);
+}
